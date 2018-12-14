43d80fe63328ea344a384e41206cd07546aa36c0
jagjeet <jagjeetsingh2019@gmail.com>
2018-12-15 00:42:21 +0530

    Added drag functionality

--- javascript/main.controller.js
+++ javascript/main.controller.js
@@ -2,7 +2,7 @@ var app = angular.module("myApp");
 
 app.controller("mainCtrl", mainController);
 
-function mainController($scope, bloodData){
+function mainController($scope, bloodData) {
     $scope.blood_data = bloodData;
     $scope.required_blood_units;
     $scope.required_blood_group;
@@ -10,36 +10,57 @@ function mainController($scope, bloodData){
     $scope.isAvailableFun = isAvailableFun;
     $scope.anythingAvailable = false;
 
-    function canDonateFun(){
+    function canDonateFun() {
         $scope.blood_data.forEach(group => {
-            if(group.canDonateTo.includes($scope.required_blood_group.group)){
+            if (group.canDonateTo.includes($scope.required_blood_group.group)) {
                 group.canDonate = true;
-            }else{
+            } else {
                 group.canDonate = false;
             }
         });
         isAnythingAvailable();
     }
 
-    function isAvailableFun(){
+    function isAvailableFun() {
         $scope.blood_data.forEach(group => {
-            if($scope.required_blood_units <= group.count){
+            if ($scope.required_blood_units <= group.count) {
                 group.isAvailable = true;
-            }else{
+            } else {
                 group.isAvailable = false;
             }
         });
         isAnythingAvailable();
     }
 
-    function isAnythingAvailable(){
+    function isAnythingAvailable() {
         var flag = false;
         $scope.blood_data.forEach(group => {
-            if(group.canDonate===true && group.isAvailable===true){
+            if (group.canDonate === true && group.isAvailable === true) {
                 flag = true;
             }
         });
         $scope.anythingAvailable = flag;
+    }
+
+
+
+    $scope.dropped = function (dragEl, dropEl) {
+
+        var drop = angular.element(dropEl);
+        var drag = angular.element(dragEl);
+
+        imgSrc = drag.attr("data-img");
+        index = drag.attr("data-grp");
+        $scope.blood_data[index].count -= $scope.required_blood_units;
+
+        drop.attr('style', 'background-image: url("' + imgSrc + '");');
+        drop.addClass('dropped-bottle');
+        drop.addClass('green-container');
+        document.querySelector('.bottle-count').style.color = 'rgb(100,100,100)';
+        document.querySelector('.bottle-count').innerHTML = $scope.required_blood_units;
+        $scope.isAvailableFun();
+        $scope.canDonateFun();
+        $scope.$apply();
     }
 
 }
